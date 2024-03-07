395a0099347720d33b6bc27d231bcbbd4619d000
Sarah Naas <sjnaas@gmail.com>
2024-03-07 22:11:50 +0100

    initial commit

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Why can I not see this?</Text>
+    </View>
+  );
+};
+
+export default Home;
