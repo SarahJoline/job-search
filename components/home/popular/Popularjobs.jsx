940eb3ae866ad0289449694710af117efe9a815e
Sarah Naas <sjnaas@gmail.com>
2024-03-12 20:43:32 +0100

    popular jobs component

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { FlatList, Pressable, Text, View } from 'react-native';
+import { ActivityIndicator } from 'react-native-web';
+import { COLORS, SIZES } from '../../../constants';
+import useFetch from '../../../hook/useFetch';
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import styles from './popularjobs.style';
 
-import styles from './popularjobs.style'
 
 const Popularjobs = () => {
+  const {isLoading, error, data } = useFetch('search', {
+    query: 'React Developer'
+  });
+
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+       <View style={styles.header}>
+      <Text style={styles.headerTitle}>Popularjobs</Text>
+      <Pressable>
+        <Text style={styles.headerBtn}>Show all</Text>
+      </Pressable>
+      </View>
+      <View style={styles.cardsContainer}>
+        {isLoading ? <ActivityIndicator size="large" color={COLORS.primary}/> : error ? <Text>Something went wrong.</Text> : <FlatList data={data} renderItem={({item}) =>  (<PopularJobCard item={item}/>)}
+      keyExtractor={item => item?.job_id}
+      contentContainerStyle={{columnGap: SIZES.medium}}
+      horizontal
+      />}
+      </View>
     </View>
   )
 }
