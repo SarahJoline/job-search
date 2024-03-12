021d99bb104fd791758e6af466e768e31a48f8ca
Sarah Naas <sjnaas@gmail.com>
2024-03-12 20:54:03 +0100

    PopularJobCard

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { Image, Pressable, Text, View } from 'react-native'
 
 import styles from './popularjobcard.style'
 
-const PopularJobCard = () => {
+const PopularJobCard = ({item, selectedJob}) => {
   return (
-    <View>
-      <Text>PopularJobCard</Text>
-    </View>
+    <Pressable style={styles.container(selectedJob, item)}>
+      <Pressable style={styles.logoContainer(selectedJob, item)}><Image source={{uri: item.employer_logo}} resizeMode='contain' style={styles.logoImage}/></Pressable>
+      <Text style={styles.companyName} numberOfLines={1}>
+        {item.employer_name}
+      </Text>
+      <View style={styles.infoContainer}>
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+          {item.job_title}
+        </Text>
+        <View style={styles.infoWrapper}>
+          <Text style={styles.publisher(selectedJob, item)}>
+            {item?.job_publisher} -
+          </Text>
+          <Text style={styles.location}> {item.job_country}</Text>
+        </View>
+      </View>
+    </Pressable>
   )
 }
 
