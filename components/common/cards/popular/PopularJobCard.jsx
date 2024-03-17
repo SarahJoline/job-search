53a0bd67ea6b29be88e3e617b83e7145b2ba89f3
Sarah Naas <sjnaas@gmail.com>
2024-03-17 22:19:59 +0100

    popularjobs and card

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import { Image, Pressable, Text, View } from 'react-native'
+import React from 'react';
+import { Image, Pressable, Text, View } from 'react-native';
 
-import styles from './popularjobcard.style'
+import styles from './popularjobcard.style';
 
-const PopularJobCard = ({item, selectedJob}) => {
+import { checkImageURL } from "../../../../utils";
+
+const PopularJobCard = ({item, selectedJob,handleCardPress}) => {
   return (
-    <Pressable style={styles.container(selectedJob, item)}>
-      <Pressable style={styles.logoContainer(selectedJob, item)}><Image source={{uri: item.employer_logo}} resizeMode='contain' style={styles.logoImage}/></Pressable>
+    <Pressable
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    >
+      <Pressable style={styles.logoContainer(selectedJob, item)}>
+        <Image
+          source={{
+            uri: checkImageURL(item?.employer_logo)
+              ? item.employer_logo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
+          resizeMode='contain'
+          style={styles.logoImage}
+        />
+      </Pressable>
       <Text style={styles.companyName} numberOfLines={1}>
         {item.employer_name}
       </Text>
--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,34 +1,65 @@
-import React from 'react';
-import { FlatList, Pressable, Text, View } from 'react-native';
-import { ActivityIndicator } from 'react-native-web';
-import { COLORS, SIZES } from '../../../constants';
-import useFetch from '../../../hook/useFetch';
-import PopularJobCard from "../../common/cards/popular/PopularJobCard";
-import styles from './popularjobs.style';
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
+import { COLORS, SIZES } from "../../../constants";
+import useFetch from "../../../hook/useFetch";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import styles from "./popularjobs.style";
 
 const Popularjobs = () => {
-  const {isLoading, error, data } = useFetch('search', {
-    query: 'React Developer'
+  const router = useRouter();
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React developer",
+    num_pages: "1",
   });
+
+  const [selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = (item) => {
+    console.log(item)
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
 
   return (
     <View style={styles.container}>
-       <View style={styles.header}>
-      <Text style={styles.headerTitle}>Popularjobs</Text>
-      <Pressable>
-        <Text style={styles.headerBtn}>Show all</Text>
-      </Pressable>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
       </View>
+
       <View style={styles.cardsContainer}>
-        {isLoading ? <ActivityIndicator size="large" color={COLORS.primary}/> : error ? <Text>Something went wrong.</Text> : <FlatList data={data} renderItem={({item}) =>  (<PopularJobCard item={item}/>)}
-      keyExtractor={item => item?.job_id}
-      contentContainerStyle={{columnGap: SIZES.medium}}
-      horizontal
-      />}
+        {isLoading ? (
+          <ActivityIndicator size='large' color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList
+            data={data}
+            renderItem={({ item }) => (
+              <PopularJobCard
+                item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
+              />
+            )}
+            keyExtractor={(item) => item.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )}
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default Popularjobs
+export default Popularjobs;
